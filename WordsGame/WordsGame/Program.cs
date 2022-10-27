c295b9adbda7a9833e786e8c80f45818902fa9f7
tommy812 <39319157+tommy812@users.noreply.github.com>
2022-10-27 02:50:29 +0100

    first prototype

--- WordsGame/WordsGame/Program.cs
+++ WordsGame/WordsGame/Program.cs
@@ -10,12 +10,16 @@ namespace WordsGame
     { 
         public const int EASY = 0, HARD = 1;
          
-        public static int nGuess,lifes,nWords,nLetters;
+        public static int nGuess,lifes,nWords,nLetters, end;
+
+        public static string input;
+
         public static IList words = System.IO.File.ReadLines("dictionary.txt").ToList();
 
 
         public static void Main(string[] args)
         {
+            end = 0;
             Console.WriteLine("Hello World!");
             
             //for the basic difficulty:
@@ -24,11 +28,44 @@ namespace WordsGame
             Random ran = new Random();
             int word = ran.Next(nWords);
             Console.WriteLine(words[word]);
+
+            int wordLenght = words[word].ToString().Length;
+            int lifes = wordLenght * 2;
+            Console.WriteLine("lifes available = "+lifes);
+
             string s = words[word].ToString();
+
             char[] wordArray = s.ToCharArray();
 
+            char[] emptyWord = Enumerable.Repeat('_', wordLenght).ToArray();
 
 
+            Console.Write($"{ string.Join("", emptyWord)}");
+
+            
+            
+            for (int j=0;j <= lifes; j++) { 
+                Console.WriteLine("insert a new letter:");
+                input = Console.ReadLine();
+                
+                for (int i=0; i < wordLenght; i++) { 
+                    
+                    if (input == wordArray[i].ToString())
+                    {
+                        emptyWord[i] = char.Parse(wordArray[i].ToString());
+                    }
+                }
+                Console.Write($" { string.Join(" ", emptyWord)}");
+
+                if (emptyWord.Contains('_')==false) {
+                    Console.WriteLine("\nCongratulations");
+                    j = lifes+1;
+                }
+                if (j == lifes)
+                {
+                    Console.WriteLine("unfortunaly you lost");
+                }
+            }
         }
     }
 }
